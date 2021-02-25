e05f8b92e9e104e25ed5fa9f75ca458eed6eada6
unknown <liyinchi@qq.com>
2021-02-25 17:18:48 +0800

    租客端授权登录不用client的middle.vue

--- src/config/env.development.js
+++ src/config/env.development.js
@@ -1,7 +1,7 @@
 // 本地环境配置
 module.exports = {
   title: '租客端',
-  baseUrl: 'http://wxh5.beta.fssh.com/customer', // 项目地址
+  baseUrl: 'http://wxh5.beta.fssh.com', // 项目地址
   baseApi: '/', // 本地api请求地址,注意：如果你使用了代理，请设置成'/'
   APPID: 'wx474f2e40fac46316',
   APPSECRET: 'cc7fd8e22d511b042bbee9053684433f',
--- src/config/env.production.js
+++ src/config/env.production.js
@@ -1,8 +1,8 @@
 // 正式
 module.exports = {
   title: '租客端',
-  baseUrl: 'https://pms.bchat.top/customer', // 正式项目地址
-  baseApi: 'https://pms.bchat.top/customer', // 正式api请求地址
+  baseUrl: 'https://pms.bchat.top', // 正式项目地址
+  baseApi: 'https://pms.bchat.top', // 正式api请求地址
   APPID: 'wx474f2e40fac46316',
   APPSECRET: 'cc7fd8e22d511b042bbee9053684433f',
   $cdn: 'https://www.xxxx.cn/static'
